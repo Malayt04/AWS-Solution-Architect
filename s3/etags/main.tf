c06db99e2f87d6cee316f4fb0106a9a58ccc8030
Malay Tiwari <malayt04@gmail.com>
2025-01-02 03:35:51 +0000

    terraform configurations

---
+++ s3/etags/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.82.2"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "example" {
+}
